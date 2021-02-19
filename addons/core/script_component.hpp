ecac5c2cfff733e957dc91a42c0354dd7a4421f0
Brett <brett@bmandesigns.com>
2021-02-19 01:02:57 -0600

    add component to core

---
+++ addons/core/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT core
+#define COMPONENT_BEAUTIFIED Core
+#include "\z\hatchet\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_CORE
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_CORE
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_CORE
+#endif
+
+#include "\z\hatchet\addons\main\script_macros.hpp"
