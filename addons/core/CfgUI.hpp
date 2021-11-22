5e0a686c3aa2fd346da5c1acd622b1d8da906770
Steve Zhao <ampersand38@gmail.com>
2021-11-21 23:21:30 -0500

    fuck these strings

--- addons/core/CfgUI.hpp
+++ addons/core/CfgUI.hpp
@@ -1,6 +1,6 @@
 class RVAR(EngineOn) {
   condition = QUOTE(!(isEngineOn (vehicle hatchet_player)));
-  label = CSTRING(EngineOn_Label);
+  label = "$str_action_engineon";
   radius = 0.3;
   buttonDown = QUOTE(vehicle hatchet_player engineOn true);
   action = "EngineOn";
@@ -8,7 +8,7 @@ class RVAR(EngineOn) {
 
 class RVAR(EngineOff) {
   condition = QUOTE(isEngineOn (vehicle hatchet_player));
-  label = CSTRING(EngineOff_Label);
+  label = "$str_action_engineoff";
   radius = 0.3;
   buttonDown = QUOTE(vehicle hatchet_player engineOn false);
   action = "EngineOff";
@@ -16,7 +16,7 @@ class RVAR(EngineOff) {
 
 class RVAR(LightOn) {
   condition = QUOTE(!(isLightOn (vehicle hatchet_player)));
-  label = CSTRING(LightsOn_Label);
+  label = "$STR_ACTION_LIGHTS_ON";
   radius = 0.3;
   buttonDown = QUOTE(vehicle hatchet_player setPilotLight true);
   action = "LightOn";
@@ -24,7 +24,7 @@ class RVAR(LightOn) {
 
 class RVAR(LightOff) {
   condition = QUOTE(isLightOn (vehicle hatchet_player));
-  label = CSTRING(LightsOff_Label);
+  label = "$STR_ACTION_LIGHTS_OFF";
   radius = 0.3;
   buttonDown = QUOTE(vehicle hatchet_player setPilotLight false);
   action = "LightOff";
