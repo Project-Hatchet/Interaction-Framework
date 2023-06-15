9edf455a8c91aeb66e191cfcc422c29bf6cccfd6
jonpas <jonpas33@gmail.com>
2023-06-15 19:21:20 +0200

    Fix switch sounds file paths

--- addons/hatchet_vxf_interaction/config/cfgSounds.hpp
+++ addons/hatchet_vxf_interaction/config/cfgSounds.hpp
@@ -3,49 +3,49 @@ class CfgSounds
   class vxf_Switch_Sound
   {
     name = "vxf_Switch_Sound";
-    sound[] = {"\vxf_interaction\sounds\switch.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch.wss",1,1,1};
     titles[]={};
   };
   class vxf_Switch_Sound_1
   {
     name = "vxf_Switch_Sound_1";
-    sound[] = {"\vxf_interaction\sounds\switch1.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch1.wss",1,1,1};
     titles[]={};
   };
   class vxf_Switch_Sound_2
   {
     name = "vxf_Switch_Sound_2";
-    sound[] = {"\vxf_interaction\sounds\switch2.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch2.wss",1,1,1};
     titles[]={};
   };
   class vxf_Switch_Sound_3
   {
     name = "vxf_Switch_Sound_3";
-    sound[] = {"\vxf_interaction\sounds\switch3.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch3.wss",1,1,1};
     titles[]={};
   };
   class vxf_Switch_Sound_4
   {
     name = "vxf_Switch_Sound_4";
-    sound[] = {"\vxf_interaction\sounds\switch4.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch4.wss",1,1,1};
     titles[]={};
   };
   class vxf_Switch_Sound_5
   {
     name = "vxf_Switch_Sound_5";
-    sound[] = {"\vxf_interaction\sounds\switch5.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\switch5.wss",1,1,1};
     titles[]={};
   };
   class vxf_HeavySwitch_Sound
   {
     name = "vxf_HeavySwitch_Sound";
-    sound[] = {"\vxf_interaction\sounds\heavySwitch.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\heavySwitch.wss",1,1,1};
     titles[]={};
   };
   class vxf_dial_sound
   {
     name = "vxf_dial_sound";
-    sound[] = {"\vxf_interaction\sounds\dial.wss",1,1,1};
+    sound[] = {"z\htf\hatchet_vxf_interaction\sounds\dial.wss",1,1,1};
     titles[]={};
   };
 };
