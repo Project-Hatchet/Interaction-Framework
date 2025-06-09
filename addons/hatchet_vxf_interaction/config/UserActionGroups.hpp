ba93e01a4adb3858fa20cab8a70da7f1b21867fa
Steve Zhao <ampersand38@gmail.com>
2025-06-09 19:42:35 -0400

    Change keybind category name to Hatchet Interaction Framework

--- addons/hatchet_vxf_interaction/config/UserActionGroups.hpp
+++ addons/hatchet_vxf_interaction/config/UserActionGroups.hpp
@@ -2,7 +2,7 @@ class UserActionGroups
 {
 	class vxf_interaction // Unique classname of your category.
 	{
-		name = "VXF Interaction"; // Display name of your category.
+		name = "Hatchet Interaction Framework"; // Display name of your category.
 		isAddon = 1;
 		group[] = { // List of all actions inside this category.
             "vxf_interaction_use",
