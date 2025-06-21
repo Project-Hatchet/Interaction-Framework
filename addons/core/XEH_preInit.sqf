7761fac7fea283eb64c489dfd486a384e2732cb3
Steve Zhao <ampersand38@gmail.com>
2025-06-21 19:50:09 -0400

    add fnc

--- addons/core/XEH_preInit.sqf
+++ addons/core/XEH_preInit.sqf
@@ -4,4 +4,7 @@ ADDON = false;
 //PREP_RECOMPILE_START;
 #include "XEH_PREP.hpp"
 //PREP_RECOMPILE_END;
+hct_core_animTypeMap = createHashMap;
+
 ADDON = true;
+
--- addons/core/functions/script_component.hpp
+++ addons/core/functions/script_component.hpp
@@ -1,1 +1,6 @@
 #include "\z\HCT\addons\core\script_component.hpp"
+
+#define ANIMTYPE_NONE 0
+#define ANIMTYPE_ANIM 1
+#define ANIMTYPE_ASRC 2
+#define ANIMTYPE_DOOR 3
